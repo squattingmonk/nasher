66e9d05f40146520826392ab2edec2a45415d6d8
Michael A. Sinclair <squattingmonk@gmail.com>
2019-07-20 04:49:14 -0500

    Update to 0.4.0

--- src/nasher.nim
+++ src/nasher.nim
@@ -2,7 +2,7 @@ import nasher/[init, list, config, unpack, convert, compile, pack, install]
 import nasher/utils/[cli, options, shared]
 
 const
-  nasherVersion = "0.3.0"
+  nasherVersion = "0.4.0"
 
   helpAll = """
   nasher: a build tool for Neverwinter Nights projects
