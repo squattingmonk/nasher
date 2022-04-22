f0e6ea86c25b70666578ee4d243f82f28890251e
Michael A. Sinclair <squattingmonk@gmail.com>
2022-04-22 14:40:20 -0500

    refactor: return packages as table of targets
    Parsing a pakcage file now returns an OrderedTableRef instead of a seq.
    This makes it easier to look up targets by name.
    
    pkgparser.nim has been merged into target.nim. Tests have been merged as
    well.

--- src/nasher/utils/target.nim
+++ src/nasher/utils/target.nim
@@ -1,29 +1,181 @@
-from sequtils import filterIt
+import os, parsecfg, streams, strutils, tables
+export tables
 
 type
+  PackageError* = object of CatchableError
+    ## Raised when the package parser encounters an error
+
   Target* = ref object
     name*, description*, file*, branch*, modName*, modMinGameVersion*: string
     includes*, excludes*, filters*, flags*: seq[string]
     rules*: seq[Rule]
 
   Rule* = tuple[pattern, dest: string]
-    
+
+const validTargetChars = {'\32'..'\64', '\91'..'\126'} - invalidFileNameChars
+
 proc `==`*(a, b: Target): bool =
   result = true
   for _, valA, valB in fieldPairs(a[], b[]):
     if valA != valB:
       return false
 
-proc getTarget*(targets: seq[Target], name: string): Target =
-  ## Returns the first target in `targets` that is named `name`.
-  for target in targets:
-    if target.name == name:
-      return target
+proc `[]`*(t: OrderedTableRef[string, Target]; index: Natural): Target =
+  ## Returns the `Target` at `index` in `t`. Raises an `IndexDefect` if there
+  ## are not `index + 1` items in `t`.
+  if t.len <= index:
+    raise newException(IndexDefect, "index $1 not in 0..$2" % [$index, $(t.len - 1)])
+  var idx: Natural
+  for value in t.values:
+    if idx == index:
+      return value
+    idx.inc
 
-proc getTargets*(targets: seq[Target], names: seq[string]): seq[Target] =
-  ## Returns all targets in `targets` that match a name in `names`. If the name
-  ## "all" is present in `names`, will return all targets.
-  if "all" in names or targets.len == 0:
-    return targets
-  result = targets.filterIt(it.name in names)
+proc raisePackageError(msg: string) =
+  ## Raises a `PackageError` with the given message.
+  raise newException(PackageError, msg)
 
+proc raisePackageError(p: CfgParser, msg: string) =
+  ## Raises a `PackageError` with the given message. Includes file, column, and
+  ## line information for the user.
+  raise newException(PackageError, "Error parsing $1($2:$3): $4" %
+    [p.getFilename, $p.getLine, $p.getColumn, msg])
+
+proc addTarget(targets: OrderedTableRef[string, Target], target, defaults: Target, filename = "") =
+  ## Adds `target` to `targets`. Missing fields other than `name` and
+  ## `description` are copied from `defaults`. A `PackageError` is raised if the
+  ## target does not have a name. `filename` is used for error messages.
+  for key, targetVal, defaultVal in fieldPairs(target[], defaults[]):
+    if targetVal.len == 0:
+      case key:
+      of "name":
+        raisePackageError("Error parsing $1: target $2 does not have a name" %
+          [filename, $(targets.len + 1)])
+      of "description":
+        discard
+      else:
+        targetVal = defaultVal
+  targets[target.name] = target
+
+proc parseCfgPackage(s: Stream, filename = "nasher.cfg"): OrderedTableRef[string, Target] =
+  ## Parses the content of `s` into a table of `Target`s where the key is the
+  ## name of the target. The cfg package format is assumed. `filename` is used
+  ## for error messages only. Raises `PackageError` if an error is encountered
+  ## during parsing.
+  result = newOrderedTable[string, Target]()
+  var
+    p: CfgParser
+    context, section: string
+    defaults = new Target
+    target = new Target
+
+  p.open(s, filename)
+  while true:
+    var e = p.next
+    case e.kind
+    of cfgEof:
+      if context == "target":
+        result.addTarget(target, defaults)
+      break
+    of cfgSectionStart:
+      # echo "Section: [$1]" % e.section
+      case e.section.toLower
+      of "package":
+        if section == "package":
+          p.raisePackageError("duplicate [package] section")
+        elif section.len > 0:
+          p.raisePackageError("[package] section must be declared before other sections")
+        context = "package"
+      of "target":
+        case context
+        of "package", "":
+          defaults = target
+        of "target":
+          result.addTarget(target, defaults)
+        else: assert(false)
+        target = new Target
+        context = "target"
+      of "sources", "rules":
+        discard
+      of "package.sources", "package.rules":
+        if context in ["target"]:
+          p.raisePackageError("[$1] must be declared within [package]" % e.section)
+      of "target.sources", "target.rules":
+        if context in ["package", ""]:
+          p.raisePackageError("[$1] must be declared within [target]" % e.section)
+      else:
+        p.raisePackageError("invalid section [$1]" % e.section)
+
+      # Trim context from subsection
+      section = e.section.toLower.rsplit('.', maxsplit = 1)[^1]
+    of cfgKeyValuePair, cfgOption:
+      # echo "Option: $1 = $2 [$1]" % [e.key, e.value.escape]
+      case section
+      of "package", "target":
+        case e.key
+        of "name":
+          if section == "target":
+            if e.value == "all":
+              p.raisePackageError("invalid target name \"all\"")
+            for c in e.value:
+              if c notin validTargetChars:
+                p.raisePackageError("invalid character $1 in target name $2" %
+                                    [escape($c), e.value.escape])
+            if e.value in result:
+              p.raisePackageError("duplicate target name $1" % e.value.escape)
+            else:
+              target.name = e.value
+        of "description": target.description = e.value
+        of "file": target.file = e.value
+        of "branch": target.branch = e.value
+        of "modName": target.modName = e.value
+        of "modMinGameVersion": target.modMinGameVersion = e.value
+        of "flags": target.flags.add(e.value)
+        # Keep for backwards compatibility, but prefer [{package,target}.sources]
+        of "source", "include": target.includes.add(e.value)
+        of "exclude": target.excludes.add(e.value)
+        of "filter": target.filters.add(e.value)
+        # Unused, but kept for backwards compatibility
+        of "version", "url", "author": discard
+        else:
+          # For backwards compatibility, treat any unknown keys as unpack rules.
+          # Unfortunately, this prevents us from detecting incorrect keys, so
+          # nasher may work unexpectedly. In the future, we will issue a
+          # deprecation warning here.
+          target.rules.add((e.key, e.value))
+      of "sources":
+        case e.key
+        of "include": target.includes.add(e.value)
+        of "exclude": target.excludes.add(e.value)
+        of "filter": target.filters.add(e.value)
+        else:
+          p.raisePackageError("invalid key $1 for section [$2$3]" %
+            [e.key.escape, if context.len > 0: context & "." else: "", section])
+      of "rules":
+        target.rules.add((e.key, e.value))
+      else:
+        discard
+    of cfgError:
+      p.raisePackageError(e.msg)
+  close(p)
+
+proc parsePackageString*(s: string, filename = "nasher.cfg"): OrderedTableRef[string, Target] =
+  ## Parses `s` into a series of targets. The parser chosen is based on
+  ## `filename`'s extension'.
+  let stream = newStringStream(s)
+  case filename.splitFile.ext
+  of ".cfg":
+    result = parseCfgPackage(stream, filename)
+  else:
+    raisePackageError("Unable to determine package parser for $1" % filename)
+
+proc parsePackageFile*(filename: string): OrderedTableRef[string, Target] =
+  ## Parses the file `filename` into a sequence of targets. The parser chosen is
+  ## based on the file's extension.
+  let fileStream = newFileStream(filename)
+  if fileStream.isNil:
+    raise newException(IOError, "Could not read package file $1" % filename)
+
+  case filename.splitFile.ext
+  of ".cfg": result = parseCfgPackage(fileStream, filename)
+  else: raisePackageError("Unable to determine package parser for $1" % filename)
--- tests/test_target.nim
+++ tests/test_target.nim
@@ -1,6 +1,21 @@
-import unittest
+import unittest, os
+from strutils import contains, `%`
 
 import nasher/utils/target
+
+proc dumpTarget(target: Target) =
+  ## Helps debug failing tests
+  echo "Target: ", target.name
+  for key, value in fieldPairs(target[]):
+    echo "  ", key, ": ", value
+
+template checkErrorMsg(msg: string, body: untyped) =
+  ## Runs `body` and checks that any raised exception message contains `msg`.
+  try:
+    body
+  except:
+    check msg in getCurrentExceptionMsg()
+    raise
 
 suite "Target type":
   test "Target equivalence":
@@ -14,24 +29,447 @@ suite "Target type":
       Target(name: "foo") != Target(name: "bar")
       Target(name: "foo", file: "bar") != Target(name: "foo", file: "baz")
 
-  test "Get target by name":
-    let
-      foo = Target(name: "foo")
-      bar = Target(name: "bar")
-      baz = Target(name: "baz")
-      targets = @[foo, bar, baz]
-      empty: seq[Target] = @[]
+  test "Get Target by index":
+    let targets = {"foo": Target(name: "foo"), "bar": Target(name: "bar")}.newOrderedTable
 
     check:
-      targets.getTarget("foo") == foo
-      targets.getTarget("bar") == bar
-      targets.getTargets(@["all"]) == targets
-      targets.getTargets(@[]) == empty
+      targets.len == 2
+      targets[0].name == "foo"
+      targets[1].name == "bar"
 
+    expect IndexDefect:
+      discard targets[2]
 
+suite "nasher.cfg parsing":
+  test "Error on malformed syntax":
+    expect PackageError:
+      discard parsePackageString("""
+      [package
+      name = "foo"
+      """)
 
+  test "Empty package file yields empty target list":
+    check parsePackageString("").len == 0
 
+  test "No targets defined yields empty target list":
+    check:
+      parsePackageString("""
+      [package]
+      name = "foo"
+      file = "bar"
+      """).len == 0
 
+  test "Error if [package] section occurs after other sections":
+    expect PackageError:
+      checkErrorMsg "[package] section must be declared before other sections":
+        discard parsePackageString("""
+        [sources]
+        [package]
+        """)
 
+  test "Error on duplicate [package] section":
+    expect PackageError:
+      checkErrorMsg "duplicate [package] section":
+        discard parsePackageString("""
+        [package]
+        [package]
+        """)
 
+  test "No error if [package] section absent":
+    let targets = parsePackageString("""
+    [target]
+    name = "foo"
+    """)
+    check: targets.len == 1
 
+  test "[sources] and [rules] can be top-level sections":
+    check parsePackageString("""
+    [sources]
+    [rules]
+
+    [target]
+    name = "foo"
+    """).len == 1
+
+  test "[sources] and [rules] sections can belong to [package] or [target]":
+    let targets = parsePackageString("""
+    [package]
+
+      [sources]
+      include = "foo"
+
+      [rules]
+      foo = "foo"
+
+    [target]
+    name = "bar"
+
+      [sources]
+      include = "bar"
+
+      [rules]
+      bar = "bar"
+
+    [target]
+    name = "foo"
+    """)
+
+    check:
+      targets.len == 2
+      targets[0].includes == @["bar"]
+      targets[0].rules == @[(pattern: "bar", dest: "bar")]
+
+      targets[1].includes == @["foo"]
+      targets[1].rules == @[(pattern: "foo", dest: "foo")]
+
+  test "[package.{sources,rules}] must be declared inside [package] or at top-level":
+    check parsePackageString("""
+    [package]
+      [package.sources]
+      [package.rules]
+    """).len == 0
+
+    check parsePackageString("""
+    [package.sources]
+    [package.rules]
+    """).len == 0
+
+    for section in ["sources", "rules"]:
+      expect PackageError:
+        checkErrorMsg "[package.$1] must be declared within [package]" % section:
+          discard parsePackageString("""
+          [target]
+          name = "foo"
+            [package.$1]
+          """ % section)
+
+  test "[target.{sources,rules}] must be declared inside [target]":
+    check parsePackageString("""
+    [target]
+    name = "foo"
+      [target.sources]
+      [target.rules]
+    """).len == 1
+
+    for section in ["sources", "rules"]:
+      expect PackageError:
+        checkErrorMsg "[target.$1] must be declared within [target]" % section:
+          discard parsePackageString("""
+          [package]
+          name = "foo"
+          [target.$1]
+          """ % section)
+
+  test "Error on unknown key in [*.sources]":
+    for context in ["package", "target"]:
+      expect PackageError:
+        checkErrorMsg "invalid key \"foo\" for section [$1.sources]" % context:
+          discard parsePackageString("""
+          [$1]
+          name = "foo"
+
+            [$1.sources]
+            foo = "bar"
+          """ % context)
+
+      expect PackageError:
+        checkErrorMsg "invalid key \"foo\" for section [$1.sources]" % context:
+          discard parsePackageString("""
+          [$1]
+          name = "foo"
+
+            [sources]
+            foo = "bar"
+          """ % context)
+
+  test "One target per [target] section":
+    let targets = parsePackageString("""
+    [package]
+    name = "foo"
+    file = "bar.mod"
+
+    [target]
+    name = "one"
+
+    [target]
+    name = "two"
+    """)
+
+    check:
+      targets.len == 2
+      targets[0].name == "one"
+      targets[1].name == "two"
+
+  test "Error if target has no name":
+    expect PackageError:
+      checkErrorMsg "target 1 does not have a name":
+        discard parsePackageString("""
+        [target]
+        file = "foo.mod"
+        """)
+
+    expect PackageError:
+      checkErrorMsg "target 2 does not have a name":
+        discard parsePackageString("""
+        [target]
+        name = "foo"
+
+        [target]
+        file = "bar.mod"
+        """)
+
+  test "Error on invalid target name":
+    expect PackageError:
+      checkErrorMsg "invalid target name":
+        discard parsePackageString("""
+        [target]
+        name = "all"
+        """)
+
+    expect PackageError:
+      checkErrorMsg "invalid character \"F\" in target name \"Foo\"":
+        discard parsePackageString("""
+        [target]
+        name = "Foo"
+        """)
+
+  test "Error on duplicate target name":
+    let pkg = parsePackageString("""
+      [package]
+      name = "foo"
+
+      [target]
+      name = "foo"
+      """)
+    check:
+      pkg.len == 1
+      pkg[0].name == "foo"
+
+    expect PackageError:
+      checkErrorMsg "duplicate target name":
+        discard parsePackageString("""
+          [target]
+          name = "foo"
+
+          [target]
+          name = "foo"
+          """)
+
+  test "{package,target}.{version,url,author} fields ignored":
+    check:
+      parsePackageString("""
+      [package]
+      version = "0.1.0"
+      url = "www.example.com"
+      author = "John Doe <johndoe@example.com>"
+
+      [target]
+      name = "foo"
+      version = "0.1.0"
+      url = "www.example.com"
+      author = "John Doe <johndoe@example.com>"
+      """).len == 1
+
+  test "target.{name,description} not inherited from [package]":
+    expect PackageError:
+      checkErrorMsg "target 1 does not have a name":
+        discard parsePackageString("""
+        [package]
+        name = "foo"
+
+        [target]
+        """)
+
+    let target = parsePackageString("""
+    [package]
+    name = "foo"
+    description = "bar"
+
+    [target]
+    name = "bar"
+    """)[0]
+    check:
+      target.description == ""
+
+  test "target.{file,branch,modName,modMinGameVersion} inherit from [package]":
+    let targets = parsePackageString("""
+    [package]
+    file = "foo.mod"
+    branch = "master"
+    modName = "foobar"
+    modMinGameVersion = "1.73"
+
+    [target]
+    name = "no-inherit"
+    file = "bar.mod"
+    branch = "baz"
+    modName = "qux"
+    modMinGameVersion = "1.69"
+
+    [target]
+    name = "inherit"
+    """)
+    check:
+      targets.len == 2
+      targets[0].file == "bar.mod"
+      targets[0].branch == "baz"
+      targets[0].modName == "qux"
+      targets[0].modMinGameVersion == "1.69"
+
+      targets[1].file == "foo.mod"
+      targets[1].branch == "master"
+      targets[1].modName == "foobar"
+      targets[1].modMinGameVersion == "1.73"
+
+  test "target.{includes,excludes,filters,flags} inherit from [package]":
+    let targets = parsePackageString("""
+    [package]
+    include = "foo/*"
+    exclude = "bar/*"
+    filter = "*.ncs"
+    flags = "--foo"
+
+    [target]
+    name = "bar"
+    include = "bar/*"
+    exclude = "foo/*"
+    filter = "*.ndb"
+    flags = "--bar"
+
+    [target]
+    name = "foo"
+    """)
+    check:
+      targets.len == 2
+      targets[0].name == "bar"
+      targets[0].includes == @["bar/*"]
+      targets[0].excludes == @["foo/*"]
+      targets[0].filters == @["*.ndb"]
+      targets[0].flags == @["--bar"]
+      targets[1].name == "foo"
+      targets[1].includes == @["foo/*"]
+      targets[1].excludes == @["bar/*"]
+      targets[1].filters == @["*.ncs"]
+      targets[1].flags == @["--foo"]
+
+  test "target.{includes,excludes,filters,flags} values added to seq when seen multiple times":
+    let target = parsePackageString("""
+      [target]
+      name = "foo"
+      include = "foo"
+      include = "bar"
+      exclude = "baz"
+      exclude = "qux"
+      filter = "*.foo"
+      filter = "*.bar"
+      flags = "--foo"
+      flags = "--bar"
+      """)[0]
+    check:
+      target.includes == @["foo", "bar"]
+      target.excludes == @["baz", "qux"]
+      target.filters == @["*.foo", "*.bar"]
+      target.flags == @["--foo", "--bar"]
+
+  test "Unpack rules added to seq":
+    let target = parsePackageString("""
+    [rules]
+    foo = "foo/"
+    bar = "bar/"
+
+    [target]
+    name = "foo"
+    """)[0]
+    check:
+      target.rules == @[(pattern: "foo", dest: "foo/"), (pattern: "bar", dest: "bar/")]
+
+  test "Unpack rules inherited from [package]":
+    let targets = parsePackageString("""
+    [rules]
+    foo = "foo/"
+
+    [target]
+    name = "bar"
+    bar = "bar/"
+
+    [target]
+    name = "foo"
+    """)
+    check:
+      targets.len == 2
+      targets[0].rules == @[(pattern: "bar", dest: "bar/")]
+      targets[1].rules == @[(pattern: "foo", dest: "foo/")]
+
+  test "Unknown keys in [target] treated as unpack rules":
+    let target = parsePackageString("""
+    [target]
+    name = "foo"
+    foo = "foo/"
+    bar = "bar/"
+    """)[0]
+    check:
+      target.rules == @[(pattern: "foo", dest: "foo/"), (pattern: "bar", dest: "bar/")]
+
+  test "Parse package string":
+    let pkg = """
+      [package]
+      name = "Test Package"
+      description = "The package description"
+
+        [package.sources]
+        include = "src/**/*.{nss,json}"
+
+        [package.rules]
+        "*" = "src"
+
+      [target]
+      name = "module"
+      file = "core_framework.mod"
+      description = "A demo module"
+
+      [target]
+      name = "erf"
+      file = "core_framework.erf"
+      description = "An importable erf"
+
+        [target.sources]
+        exclude = "src/test_*.nss"
+        exclude = "src/_*.nss"
+        filter = "*.ncs"
+      """
+
+    let
+      targetA = Target(name: "module",
+                       file: "core_framework.mod",
+                       description: "A demo module",
+                       includes: @["src/**/*.{nss,json}"],
+                       rules: @[(pattern: "*", dest: "src")])
+      targetB = Target(name: "erf",
+                       file: "core_framework.erf",
+                       description: "An importable erf",
+                       includes: @["src/**/*.{nss,json}"],
+                       excludes: @["src/test_*.nss", "src/_*.nss"],
+                       filters: @["*.ncs"],
+                       rules: @[(pattern: "*", dest: "src")])
+      manual = {"module": targetA, "erf": targetB}.newOrderedTable
+      parsed = parsePackageString(pkg)
+
+    check manual == parsed
+
+suite "nasher.cfg backwards compatibility":
+  const
+    dir = "tests/corpus"
+    packages = @[
+      "https://github.com/squattingmonk/nasher", ## Example from readme
+      "https://github.com/squattingmonk/sm-utils",
+      "https://github.com/squattingmonk/nwn-core-framework",
+      "https://github.com/tinygiant/darksun-sot",
+      "https://github.com/tinygiant98/darksun-resources",
+      "https://github.com/Kaikas/mintarn",
+      "https://github.com/b5635/the-frozen-north"]
+
+  # Since we know that cfg files parse correctly now, we only have to check
+  # whether these files throw exceptions.
+  for package in packages:
+    test package:
+      discard parsePackageFile(dir / package.lastPathPart.addFileExt("cfg"))
